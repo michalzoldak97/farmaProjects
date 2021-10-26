1bb882b10d54c38490d27c1b8d8dab26acfeacc6
michalzoldak97 <michalzoldak97@gmail.com>
2021-10-26 18:27:44 +0200

    forkin calc

--- farmaProject2/solution.py
+++ farmaProject2/solution.py
@@ -1,38 +1,35 @@
 import sys
-
-
-def _get_description(infile):
-    description_str = []
-    for char in infile:
-        description_str.append(char.replace('\n', '').split(' '))
-    description = []
-    for num in description_str:
-        new_col = []
-        for par in num:
-            new_col.append(float(par))
-        description.append(new_col)
-    return description
+import fileinput
 
 
 def _determine_val(description, test_var):
     test_var[:0] = [1]
-    res = 0
+    res = 0.0
     for stat in description[1:]:
-        comp = 1
-        for coeff in stat[:(len(stat)-1)]:
-            if coeff == 0:
-                continue
-            comp = comp*test_var[int(coeff)]
-        comp = comp*stat[-1]
+        comp = 1.0
+        if len(stat) > 1:
+            for coeff in stat[:(len(stat) - 1)]:
+                if int(coeff) == 0:
+                    continue
+                comp = comp * test_var[int(coeff)]
+        comp = comp * stat[-1]
         res = res + comp
     return res
 
 
 def calculate_res():
-    description = _get_description(open('description.txt', 'r'))
+    description = []
+    try:
+        for f in (open('description1.txt', 'r')):
+            line_parts = []
+            for part in f.split(" "):
+                line_parts.append(float(part.rstrip()))
+            description.append(line_parts)
+    except:
+        description = 5.0
     params_str = []
     for in_col in sys.stdin:
-        params_str.append(in_col.replace('\n', '').split(' '))
+        params_str.append(in_col.replace("\n", "").split(" "))
 
     params = []
     for par_col in params_str:
