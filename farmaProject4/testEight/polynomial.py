f1a3d7588367ef9c27d1524b61fc9fa078256b01
michalzoldak97 <michalzoldak97@gmail.com>
2021-12-01 22:46:28 +0100

    remote

---
+++ farmaProject4/testEight/polynomial.py
@@ -0,0 +1,80 @@
+class PolynomialFeaturesForDegree:
+    
+    def __init__(self, deg):
+        self.deg = deg
+        self.opts = []
+
+    def _get_terms(self, feats):
+        m = len(feats[0])
+        num = (self.deg + 1) ** m
+        ls = [0.0] * m 
+        
+        for x in range(1, num):
+            for idx in range(len(ls)-1, -1, -1):
+                if ls[idx] == 0:
+                    ls[idx] += 1
+                    break
+                elif ls[idx] % self.deg == 0:
+                    ls[idx] = 0
+                    if ls[idx - 1] != self.deg:
+                        ls[idx - 1] += 1
+                        break
+                    else:
+                        dfc = -1
+                        stop = False
+                        while not stop and (idx + dfc) > -1:
+                            if ls[idx + dfc] != self.deg:
+                                ls[idx + dfc] += 1
+                                stop = True
+                            else:
+                                ls[idx + dfc] = 0
+                                dfc -= 1
+                        break
+                elif idx == (len(ls) - 1):
+                    ls[idx] += 1
+                    break
+            
+            if sum(ls) == self.deg:
+                self.opts.append(tuple(ls))
+
+    def get_polynomial_features(self, feats):
+        self._get_terms(feats)
+        poly_features = [[] for _ in enumerate(feats)]
+        feature_ls = []
+        
+        for i in range(len(feats[0])):
+            to_add = [row[i] for row in feats]
+            feature_ls.append(to_add)
+
+        for i, tup in enumerate(self.opts):
+            vec = [1. for _ in enumerate(feats)]
+            
+            for feature_idx, exp in enumerate(tup):
+                for j, el in enumerate(vec):
+                    vec[j] = el * (feature_ls[feature_idx][j] ** exp)
+
+            for k, _ in enumerate(poly_features):
+                poly_features[k].append(vec[k])
+                
+        return poly_features
+
+
+class PolynomialFeatures:
+
+    def __init__(self, deg):
+        self.deg = deg
+        self.trnsf = []
+        
+    def fit(self):
+        for i in range(1, self.deg + 1):
+            self.trnsf.append(PolynomialFeaturesForDegree(i))
+        
+    def transform(self, dataset):
+        self.fit()
+        temp = [[1.0] for _ in enumerate(dataset)]
+        for t in self.trnsf:
+            features_for_deg = t.get_polynomial_features(dataset)
+            for i, _ in enumerate(temp):
+                for el in features_for_deg[i]:
+                    temp[i].append(el)
+        return temp
---
+++ farmaProject4/testEight/regressor.py
@@ -0,0 +1,37 @@
+from polyoperations import PolyRegressor
+import utils as util
+import copy
+
+
+def train(reglr: list):
+    global min_degree, max_degree, x_tr, y_tr, x_vl, y_vl, res_tpl
+    for rg in reglr:
+        test_res = [0, 0.0]
+        for deg in range(min_degree, max_degree + 1):
+            a, b, c, d = copy.deepcopy(x_tr), copy.deepcopy(y_tr), copy.deepcopy(x_vl), copy.deepcopy(y_vl)
+            poly_reg = PolyRegressor(a, c, b, d, degree=deg, iter_=5000, reg=rg)
+            res = poly_reg.calc_polynomial()[2]
+            if deg > min_degree and res < test_res[1]:
+                test_res = [deg, res]
+            elif deg == min_degree:
+                test_res = [deg, res]
+        if test_res[1] < res_tpl[1]:
+            res_tpl[0], res_tpl[1] = test_res[0], test_res[1]
+            res_tpl[2] = rg
+
+
+x_train, y_train = util.get_data(util.get_filepath())
+x_test = util.read_sys_file()
+
+x_tr, y_tr, x_vl, y_vl = util.train_test_split(x_train, y_train, .7)
+res_tpl = [0, 999.0, 0.0]
+
+min_degree, max_degree = 1, 12
+
+train([.0, .1, .2, .3, .4, .5])
+
+final_poly_reg = PolyRegressor(x_train, x_test, y_train, y_train, degree=res_tpl[0], iter_=10000, reg=res_tpl[2])
+final_res = final_poly_reg.calc_result()[2]
+
+for r in final_res:
+    print(r)
